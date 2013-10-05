7806f7b937027c32c8b8b1c1c8d574b16befb61a
zenbowman <zenbowman@gmail.com>
2013-10-05 09:12:26 -0700

    Exercise 3.6 complete

--- src/Roop.cpp
+++ src/Roop.cpp
@@ -22,7 +22,7 @@ bool isCommentOrWhitespace(char *lineData) {
 }
 
 bool isSkippableLine(char *lineData) {
-  return (isCommentOrWhitespace(lineData) || isDisplayCommand(lineData));
+  return isCommentOrWhitespace(lineData); 
 }
 
 int main( int argc, char** argv )
@@ -32,8 +32,9 @@ int main( int argc, char** argv )
   char _command[256];
   sexp_t* command;
   EvalResult commandResult;
+  std::string lastCommand;
   
-  namedWindow("Result", CV_WINDOW_AUTOSIZE);
+  //namedWindow("Result", CV_WINDOW_AUTOSIZE);
 
   if (argc > 1) {
     char *initScriptFilename = argv[1];
@@ -41,19 +42,27 @@ int main( int argc, char** argv )
     // Load initializer commands
     std::ifstream inputFile(initScriptFilename);
     int line_number = 0;
+    bool shouldDisplay = false;
     while (inputFile.getline(_command, 256)) {
       
       if (!isSkippableLine(_command)) {
         command = parse_sexp(_command, strlen(_command));
         if (command == NULL) {
           std::cout << "Invalid s-expression on line " << line_number << " - skipping line" << std::endl;
-        } else {
-          eval(command);
-        }
+        } else if (isDisplayCommand(_command)) {
+	  imshow(lastCommand.c_str(), commandResult.resultMat);
+	  shouldDisplay = true;
+	} else {
+	  RoopList result = eval(command);
+	  if (result.size() > 0) {
+	    commandResult = eval(command)[0];
+	    lastCommand = _command;
+	  }
+	}
         line_number ++;
       }
     }
-    
+    waitKey(0);
   }
   
   while(true) {
@@ -65,7 +74,7 @@ int main( int argc, char** argv )
       break;
     }
     else if (isDisplayCommand(_command)) {
-      imshow("Result", commandResult.resultMat);
+      imshow(lastCommand.c_str(), commandResult.resultMat);
       waitKey(0);
     }
     else {
@@ -77,6 +86,7 @@ int main( int argc, char** argv )
         RoopList result = eval(command);
         if (result.size() > 0) {
           commandResult = eval(command)[0];
+	  lastCommand = _command;
         }
         destroy_sexp(command);
       }
